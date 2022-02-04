764da964c645ae26ebd6bd05ada884162fb9a4e5
Cythna <reeedt@gmail.com>
2022-02-04 12:14:21 -0500

    Player Controller with Animations

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,57 +1,35 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
-
-onready var _animated_sprite = $AnimatedSprite
-
-var idleAnims = ["Idle_Right", "Idle_Left", "Idle_Up", "Idle_Down"]
+export var MAX_SPEED: int = 120
+export var ACCELERATION: int = 1000
+export var FRICTION: int = 1000
 
 var velocity = Vector2()
+var idle_state = true
+var is_attacking = false
 var facingDirection = Vector2()
 
-func animate_character():
-    if velocity.y < 0:
-        _animated_sprite.play("Walking_Up") 
-    elif velocity.y > 0:
-        _animated_sprite.play("Walking_Down") 
-    elif velocity.x > 0:
-        _animated_sprite.play("Walking_Right")
-    elif velocity.x < 0:
-        _animated_sprite.play("Walking_Left")
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationTreeState = animationTree.get("parameters/playback")
 
-    if velocity == Vector2.ZERO && !idleAnims.has(_animated_sprite.animation):
-        match [facingDirection]:
-            [Vector2(1,0)]:
-                _animated_sprite.play("Idle_Right") 
-                print("Right")
-            [Vector2(-1,0)]:
-                _animated_sprite.play("Idle_Left") 
-                print("Left")
-            [Vector2(0,1)]:
-                _animated_sprite.play("Idle_Up") 
-                print("Up")
-            [Vector2(0,-1)]:
-                _animated_sprite.play("Idle_Down") 
-                print("Down")
+func get_input(delta):
+	var input_vector = Vector2.ZERO 
+	input_vector.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
+	input_vector.y = Input.get_action_strength("Down") - Input.get_action_strength("Up")
+	input_vector = input_vector.normalized()
 
-func get_input():
-    velocity = Vector2()
-    if Input.is_action_pressed("Right"):
-        velocity.x += 1
-        facingDirection = Vector2(1, 0)
-    if Input.is_action_pressed("Left"):
-        velocity.x -= 1
-        facingDirection = Vector2(-1, 0)
-    if Input.is_action_pressed("Up"):
-        velocity.y -= 1
-        facingDirection = Vector2(0, 1)
-    if Input.is_action_pressed("Down"):
-        velocity.y += 1
-        facingDirection = Vector2(0, -1)
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Walk/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTreeState.travel("Walk")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		animationTreeState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
-    velocity = velocity.normalized() * speed
+	velocity = move_and_slide(velocity)
 
-func _physics_process(_delta):
-    get_input()
-    animate_character()
-    velocity = move_and_slide(velocity)
+func _physics_process(delta):
+	get_input(delta)

13289bafa20e6d45275ef0fdbbf9c513cf5d516d
Cythna <reeedt@gmail.com>
2022-02-08 18:33:25 -0500

    TileMap created, test map laid out

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,11 +9,33 @@ var idle_state = true
 var is_attacking = false
 var facingDirection = Vector2()
 
+enum {
+	MOVE,
+	DASH,
+	ATTACK
+		}
+var state = MOVE
+
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationTreeState = animationTree.get("parameters/playback")
 
+func _ready():
+	animationTree.active = true
+
+func _physics_process(delta):
+	get_input(delta)
+
 func get_input(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		DASH:
+			dash_state(delta)
+		ATTACK:
+			attack_state(delta)
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO 
 	input_vector.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
 	input_vector.y = Input.get_action_strength("Down") - Input.get_action_strength("Up")
@@ -29,7 +51,16 @@ func get_input(delta):
 		animationTreeState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
+	if Input.is_action_just_pressed("Attack_1"):
+		state = ATTACK
+
 	velocity = move_and_slide(velocity)
 
-func _physics_process(delta):
-	get_input(delta)
+func dash_state(delta):
+	pass 
+
+func attack_state(delta):
+	animationTreeState.travel("Attack")
+
+func _attack_animation_finished():
+	state = MOVE
